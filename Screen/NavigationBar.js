9283870c6f5ff7ca1f90f63398630fce8f67a5c4
shubhi_sharma <69205235+SHUBHI-SHARMA@users.noreply.github.com>
2021-06-24 15:57:49 +0530

    24th june

--- Screen/NavigationBar.js
+++ Screen/NavigationBar.js
@@ -17,6 +17,7 @@ import {
     Modal,
     TouchableHighlight                                                  
   } from 'react-native';
+  import { LinearTextGradient } from "react-native-text-gradient";
   import { Col, Row, Grid } from "react-native-easy-grid";
   import Icon from 'react-native-vector-icons/FontAwesome';
   import CircleButton from 'react-native-circle-button';
@@ -26,14 +27,21 @@ import {
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { FloatingAction } from "react-native-floating-action";
+import { FloatButton } from 'react-native-easy-floating-button';
+//import HomeScreen from './HomeScreen';
 
 
-
-
-function HomeScreen() {
+function FileScreen() {
 
 
   LogBox.ignoreLogs(['Animated: `useNativeDriver`']);
+
+  let menu = [
+    {name : 'Image',image: require("./image.png"), action : ''},
+    {name : 'Audio',image: require("./audio.png") , action : ''},
+    {name : 'Document',image:require("./docum.png"), action : ''},
+    {name : 'Video',image:require("./videoi.png"), action : ''},
+];
 
   const actions = [
     {
@@ -72,24 +80,22 @@ function HomeScreen() {
 
   return (
     <View style={styles.main}>
-    {/* <ImageBackground
-    source={require('./bggg2.png')}
+    <ImageBackground
+    source={require('./bluee.jpeg')}
     style={{height:'100%',width:'100%'}}
-    > */}
-    {/* <ImageBackground
-    source={require('./bgH.jpeg')}
-    style={{height:'100%',width:'100%'}}
-    > */}
+    >
     <Animatable.View 
             animation="bounceIn"
             style={styles.footer}
         >
-    <Text style={{top:50,color:'black',fontWeight:'bold',fontSize:25,paddingLeft:17}}>Rizky Lancelot</Text>
+    <Text style={{top:65,color:'black',fontFamily:'Orbitron-Bold',fontSize:25,paddingLeft:17}}>Rizky Lancelot</Text>
 
-    <Searchbar
+    {/* <Searchbar
       placeholder="Search"
+      onChangeText={onChangeSearch}
+      value={searchQuery}
       style={styles.search}
-    />
+    /> */}
 
  <View style={styles.space}>
  <View style={styles.logo_file}>
@@ -113,13 +119,16 @@ function HomeScreen() {
             animation="bounceIn"
             style={styles.footer}
         >
-    <Text style={{top:58,color:'black',paddingLeft:17, fontWeight:'bold', fontSize:21, fontFamily:'Roboto', textShadowColor:'grey', textShadowRadius:3}}>
+    <Text style={{top:58,color:'black',paddingLeft:17,fontFamily:'Orbitron-Bold', fontSize:21, textShadowColor:'grey', textShadowRadius:3}}>
 Categories
     </Text>
     </Animatable.View>
     <Grid style={styles.after_space}>
         <Row style={styles.row1}>
             <Col style={{paddingLeft:25}}>
+            <Animatable.View
+            animation="bounceIn"
+            >
             <View style={styles.column}>
             {/* <Feather
                     name="file"
@@ -130,12 +139,17 @@ Categories
                 source={require("./docum.png")}
                 style={{height:70,width:70,marginTop:-5}}
             />
+            
                 </View>
-                <Text style={{color:'#808080', fontWeight:'bold', textAlign:'center',paddingRight:21,top:4}}>
+                </Animatable.View>
+                <Text style={{color:'#808080',fontFamily:'Orbitron-Bold', textAlign:'center',paddingRight:21,top:4}}>
                 Document</Text>
             </Col>
             
             <Col>
+            <Animatable.View
+animation="bounceIn"
+>
             <View style={styles.column}>
             {/* <Icon
                     name="image"
@@ -146,10 +160,15 @@ Categories
                 source={require("./image.png")}
                 style={{height:70,width:70,marginTop:-9}}
             />
+            
                 </View>
-                <Text style={{color:'#808080', fontWeight:'bold', textAlign:'center',paddingRight:29,top:4}}>Images</Text>
+                </Animatable.View>
+                <Text style={{color:'#808080',fontFamily:'Orbitron-Bold', textAlign:'center',paddingRight:29,top:4}}>Images</Text>
             </Col>
             <Col style={{paddingRight:10}}>
+            <Animatable.View
+animation="bounceIn"
+>
             <View style={styles.column}>
             {/* <Feather
                     name="file"
@@ -161,12 +180,17 @@ Categories
                 style={{height:70,width:70,marginTop:-5}}
             />
                 </View>
-                <Text style={{color:'#808080', fontWeight:'bold', textAlign:'center',paddingRight:21,top:4}}>
+                </Animatable.View>
+
+                <Text style={{color:'#808080',fontFamily:'Orbitron-Bold', textAlign:'center',paddingRight:21,top:4}}>
                 Extras</Text>
             </Col>
         </Row>
         <Row style={styles.row2}>
             <Col style={{paddingLeft:25}}>
+            <Animatable.View
+animation="bounceIn"
+>
             <View style={styles.column}>
             {/* <Feather
             
@@ -179,10 +203,15 @@ Categories
                 style={{height:50,width:50}}
             />
                 </View>
-                <Text style={{color:'#808080', fontWeight:'bold', textAlign:'center',paddingRight:25,top:4}}>Audio</Text>
+                </Animatable.View>
+
+                <Text style={{color:'#808080',fontFamily:'Orbitron-Bold', textAlign:'center',paddingRight:25,top:4}}>Audio</Text>
             </Col>
             <Col>
             <View style={styles.column}>
+            <Animatable.View
+animation="bounceIn"
+>
             {/* <Feather
             
                     name="pause-circle"
@@ -193,11 +222,17 @@ Categories
                 source={require("./apk.png")}
                 style={{height:70,width:70,top:-9}}
             />
+                        </Animatable.View>
+
                 </View>
-                <Text style={{color:'#808080', fontWeight:'bold', textAlign:'center',paddingRight:25,top:4}}>APK</Text>
+
+                <Text style={{color:'#808080',fontFamily:'Orbitron-Bold', textAlign:'center',paddingRight:25,top:4}}>APK</Text>
             </Col>
             <Col>
             <View style={styles.column}>
+            <Animatable.View
+animation="bounceIn"
+>
             {/* <Feather
                     name="video"
                     color="#00b3ff"
@@ -207,8 +242,11 @@ Categories
                 source={require("./videoi.png")}
                 style={{height:100,width:90,marginTop:-25}}
             />
+                            </Animatable.View>
+
                 </View>
-                <Text style={{color:'#808080', fontWeight:'bold', textAlign:'center',fontSize:16,paddingRight:25,top:4}}>Video</Text>
+
+                <Text style={{color:'#808080',fontFamily:'Orbitron-Bold', textAlign:'center',fontSize:16,paddingRight:25,top:4}}>Video</Text>
             </Col>
             
         </Row>
@@ -219,13 +257,13 @@ Categories
             animation="fadeInUpBig"
            
         >
-        <Text style={{top:160,color:'black',paddingLeft:20,fontWeight:'bold', fontSize:20, fontFamily:'Roboto', textShadowColor:'grey', textShadowRadius:3}}>
+        <Text style={{top:175,color:'black',paddingLeft:20,fontFamily:'Orbitron-Bold', fontSize:20,textShadowColor:'grey', textShadowRadius:3}}>
 Recent Files
     </Text>
     
      
     <View style={styles.recent}>
-        <Text style={{fontWeight:'bold',fontSize:20,color:'black'}}>
+        <Text style={{fontFamily:'Orbitron-Bold',fontSize:20,color:'black'}}>
             My first.png
         </Text>
     </View>
@@ -257,7 +295,7 @@ Recent Files
      iconButtonLeft={require("./videoi.png")}
      /> */}
      
-     <FloatingAction
+     {/* <FloatingAction
      color={"#00b3ff"}
      shadow={{ shadowOpacity: 1, shadowOffset: { width: 0, height: 15 }, shadowColor: "white", shadowRadius: 20 }}
     showBackground={false}
@@ -267,7 +305,25 @@ Recent Files
     onPressItem={name => {
       console.log(`selected button: ${name}`);
     }}
-  />
+  /> */}
+
+  <FloatButton
+    buttonInitializeLeft={320}
+    buttonInitializeTop={50}
+    menuOffsetY={-100} 
+    menus={menu} 
+    menuWidth={350}
+    menuHeight={120}
+    buttonSize={60}
+    buttonImage={require('./add.png')}
+    backgroundColor={'#ffffff00'}
+    menuBackgroundColor={'#00b3ff'}
+    numberOfRow={1}
+    numberOfColumn={4}
+    menuButtonTextStyle={'Orbitron-Medium'}
+  //  menuButtonDeleteImage={require('./cross.png')}
+    moveRange={{width : 400,height:400}}
+ />
      </View>
      
      {/* </Animatable.ScrollView> */}
@@ -275,16 +331,20 @@ Recent Files
      {/* </Animatable.View> */}
      {/* </ImageBackground> */}
      {/* </ImageBackground> */}
+     </ImageBackground>
  </View>
   );
 }
+
+
+
 
 function SettingsScreen() {
   const [showModal, setShowModal] = useState(false);
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
     <ImageBackground 
-    source={require('./settings.jpeg')}
+    source={require('./settingss.jpeg')}
     style={{height:'100%',width:'100%'}}
     >
       {/* <Grid>
@@ -303,17 +363,22 @@ function SettingsScreen() {
           </Col>
         </Row>
       </Grid> */}
-      <View style={{top:45,marginLeft:345}}>
+      <View style={{top:95,marginLeft:325,paddingRight:10}}>
+      <Animatable.View
+      animation="bounceIn"
+      >
       <Feather
         name='log-out'
         size={50}
         color='#00b3ff'
+
       />
         <Text style={{fontFamily:'Orbitron-Bold',color:'#00b3ff',fontSize:14}}>Logout</Text>
+        </Animatable.View>
       </View>
       <Image 
         source={require('./users.png')}
-        style={{top:43,height:150,width:150,marginLeft:140}}
+        style={{top:123,height:100,width:100,marginLeft:170}}
       />
       {/* <View style={{top:39,height:150,width:150,marginLeft:140}}>
       <Feather
@@ -322,10 +387,13 @@ function SettingsScreen() {
         color='white'
       />
       </View> */}
-<Text style={{top:43,color:'#00b3ff',marginLeft:140,fontFamily:'Orbitron-Bold'}}>UserName</Text>
-<Text style={{top:45,color:'#00b3ff',marginLeft:140,fontFamily:'Orbitron-Bold'}}>Gender</Text>
-<Text style={{top:50,color:'#00b3ff',marginLeft:140,fontFamily:'Orbitron-Bold'}}>Peer Id:</Text>
-
+      
+<Text style={{top:123,color:'#00b3ff',marginLeft:90,fontFamily:'Orbitron-Bold'}}>UserName :</Text>
+<Text style={{top:125,color:'#00b3ff',marginLeft:90,fontFamily:'Orbitron-Bold'}}>Gender :</Text>
+<Text style={{top:130,color:'#00b3ff',marginLeft:90,fontFamily:'Orbitron-Bold'}}>Peer Id :</Text>
+<Animatable.View
+animation="bounceIn"
+>
 <View style={styles.container}>
 
 <Text style={{top:10,fontSize:20,fontFamily:'Orbitron-Bold',color:'white',marginLeft:50,marginTop:10,alignContent:'center'}}>
@@ -378,11 +446,89 @@ function SettingsScreen() {
     <Icon name='plus' size={37} color='#01a699' />
   </TouchableOpacity>
 </View>
+</Animatable.View>
+<Animatable.View
+ animation="fadeInUpBig"
+>
+<View style={styles.bottom_container}>
+<Grid>
+  <Row style={{top:65,left:40}}>
+    <Col>
+      <Text style={styles.bottom_container_text}>Image</Text>
+    </Col>
+    <Col>
+      <Text style={styles.bottom_container_text}>Document</Text>
+    </Col>
+    </Row>
+    <Row style={{top:45,left:60}}>
+    <Col>
+      <Text style={styles.bottom_container_text}>Audio</Text>
+    </Col>
+    <Col>
+      <Text style={styles.bottom_container_text}>Video</Text>
+    </Col>
+  </Row>
+</Grid>
+
+</View>
+</Animatable.View>
 
 </ImageBackground>
     </View>
   );
 }
+
+
+function HomeScreen(){
+  return(
+    <View style={{flex:1}}>
+    <ImageBackground
+    source={require('./home2.jpeg')}
+    style={{height:'100%',width:'100%'}}>
+
+    <View style={{top:140, left:30}}>
+    <Animatable.View
+    animation="slideInUp"
+    duration={1000}>
+
+<LinearTextGradient
+  style={{ fontFamily:'Orbitron-Black', fontSize:25 }}
+  locations={[0, 1]}
+  colors={["#00b3ff", "#05e395"]}
+  start={{ x: 0, y: 0 }}
+  end={{ x: 1, y: 0 }}
+>
+<Text>
+ File Manager
+ </Text>
+</LinearTextGradient>
+
+
+
+    {/* <Text style={{, color:'black'}}>File Manager</Text> */}
+    <Text style={{top:10,fontFamily:'Orbitron-Medium', fontSize:12, color:'black'}}>Let's Clean and Manage Your Files</Text>
+    </Animatable.View>
+<Text style={{transform: [{ rotate: '270deg' }],fontFamily:'Orbitron-Medium', fontSize:12, color:'#00b3ff', marginRight:50,top:-100,marginLeft:-310}}>Decentralized Storage</Text>
+<Text style={{transform: [{ rotate: '270deg' }],fontFamily:'Orbitron-Medium', fontSize:12, color:'#05e395', marginRight:50,top:20,marginLeft:-310}}>Internal Storage</Text>
+<Animatable.View
+animation="bounceInLeft"
+duration={2000}
+>
+<View style={styles.home_container}>
+
+</View>
+<View style={styles.home_container2}>
+
+</View>
+</Animatable.View>
+    </View>
+    </ImageBackground>
+    </View>
+  )
+}
+
+
+
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -396,11 +542,25 @@ export default function NavigationBar() {
       barStyle={{backgroundColor: 'white',
       }}
       >
-        <Tab.Screen name="Home" component={HomeScreen} 
+<Tab.Screen name="Home" component={HomeScreen} 
           options= {{
                 tabBarIcon: ({ color }) => (  
                    
-                        <Icon name="home" color={color} size={20}/>  
+                        <Icon name="home" color={color} size={25}/>  
+                   ),  
+                activeColor: '#00b3ff',  
+                inactiveColor: 'grey',  
+                barStyle: { backgroundColor: '#00b3ff' },  
+            } }   
+
+        />
+
+
+        <Tab.Screen name="Files" component={FileScreen} 
+          options= {{
+                tabBarIcon: ({ color }) => (  
+                   
+                        <Icon name="file" color={color} size={22}/>  
                    ),  
                 activeColor: '#00b3ff',  
                 inactiveColor: 'grey',  
@@ -412,7 +572,7 @@ export default function NavigationBar() {
            options={{
           tabBarLabel: 'Settings',
           tabBarIcon: ({ color }) => (
-            <Icon name="cogs" color={color} size={20} />
+            <Icon name="cogs" color={color} size={23} />
           ),
           activeColor:'#05e395',
           inactiveColor:'blue',
@@ -430,6 +590,27 @@ main:{
 flex:1,
 
 },
+
+home_container:{
+  height:340,
+  width:170,
+  backgroundColor:'#00b3ff',
+ top:20,
+ left:140,
+ borderRadius:40
+},
+home_container2:{
+  height:340,
+  width:150,
+  backgroundColor:'#00b3ff',
+ top:-329,
+ left:172,
+ borderBottomRightRadius:40,
+ borderTopRightRadius:40,
+borderTopLeftRadius:40,
+ opacity:0.4
+},
+
 text_space:{
   color: 'black',
   fontSize: 20,
@@ -470,19 +651,34 @@ modal: {
     elevation: 5
 },
 container:{
-  top:80,
-height:180,
+  top:140,
+height:150,
 width:320,
 backgroundColor:'#05e395',
 marginLeft:50,
 marginRight:40,
-borderBottomLeftRadius:10,
-borderBottomRightRadius:10,
-borderTopLeftRadius:10,
-borderTopRightRadius:10
+borderBottomLeftRadius:15,
+borderBottomRightRadius:15,
+borderTopLeftRadius:15,
+borderTopRightRadius:15
 },
-
-
+bottom_container:{
+top:140,
+height:270,
+width:395,
+backgroundColor:"#00b3ff",
+alignContent:'center',
+borderTopLeftRadius:50,
+borderTopRightRadius:50,
+opacity:0.8,
+marginLeft:10,
+paddingRight:20
+},
+bottom_container_text:{
+  fontSize:20,
+  fontFamily:'Orbitron-Medium',
+  color:'white'
+},
 search:{
     top:70,
     marginLeft:17,
@@ -490,7 +686,7 @@ search:{
     borderColor:"#00b3ff"
   },
   row1:{
- top:20,
+ top:30,
  alignContent:'center',
  justifyContent:'center'
   },
@@ -500,18 +696,17 @@ search:{
     justifyContent:'center',
     paddingRight:10
      },
-  column:{
-backgroundColor:'#c9e5ee',
-paddingTop:20,
-borderRadius:20,
-width:100,
-height:90,
-alignItems:'center',
-opacity:0.8,
-elevation:50,
-shadowColor:'#00b3ff'
-
-},
+     column:{
+      backgroundColor:'#ecf3f9',
+      paddingTop:20,
+      borderRadius:20,
+      width:100,
+      height:90,
+      alignItems:'center',
+      elevation:70,
+      borderColor:'#00b3ff',
+      borderWidth:0.7
+      },
 column2:{
     backgroundColor:'#c9e5ee',
     paddingTop:-12,
@@ -524,14 +719,16 @@ column2:{
     recent:{
         backgroundColor:"#00b3ff",
         alignContent:"center",
-        height:80,
-        width:350,
+        height:90,
+        width:370,
         paddingLeft:20,
         marginLeft:20,
         marginRight:10,
         borderRadius:10,
         opacity:0.2,
-        top:170
+        top:190,
+        borderColor:'white',
+        borderWidth:1
     },
 
 space2:{
